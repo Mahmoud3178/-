d26448a0c93e59b00f370f140a7e26a92ac8f646
Mahmoud3178 <nmm3598@gmail.com>
2025-09-17 23:32:03 +0300

    Update API rewrites and vercel config26

--- src/app/DTOS/update-profile-user.dto.ts
+++ src/app/DTOS/update-profile-user.dto.ts
@@ -1,7 +1,7 @@
 export interface UpdateProfileUser {
-  id: string;         // ⬅️ ضروري
+  id: string;
   name: string;
   phoneNumber: string;
   email: string;
-  
+  imageUrl?: string;  // ← لو عايزها، أضفها هنا
 }
